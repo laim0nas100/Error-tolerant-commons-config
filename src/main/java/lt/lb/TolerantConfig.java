d7350d16df43482123adf374edce769b205380a4
laim0nas100 <aciukadsiunciate@gmail.com>
2023-03-08 15:52:22 +0200

    add asProperties method

--- src/main/java/lt/lb/TolerantConfig.java
+++ src/main/java/lt/lb/TolerantConfig.java
@@ -31,25 +31,25 @@ import org.apache.commons.configuration2.tree.ExpressionEngine;
  * @param <Conf>
  */
 public interface TolerantConfig<Conf extends ImmutableConfiguration> extends ImmutableConfiguration {
-    
+
     public static class DefaultTolerantConfig<C extends ImmutableConfiguration> implements TolerantConfig<C> {
-        
+
         protected final ConfigurationSupplier<C> supply;
-        
+
         public DefaultTolerantConfig(ConfigurationSupplier<C> supply) {
             this.supply = Objects.requireNonNull(supply, "ConfigurationSupplier must not be null");
         }
-        
+
         @Override
         public C getDelegated() {
             return supply.getConfOrNull();
         }
-        
+
         @Override
         public int hashCode() {
             return Objects.hashCode(getDelegated());
         }
-        
+
         @Override
         public boolean equals(Object obj) {
             if (this == obj) {
@@ -64,33 +64,33 @@ public interface TolerantConfig<Conf extends ImmutableConfiguration> extends Imm
             final DefaultTolerantConfig<?> other = (DefaultTolerantConfig<?>) obj;
             return Objects.equals(this.getDelegated(), other.getDelegated());
         }
-        
+
     }
-    
+
     public static final TolerantConfig empty = new DefaultTolerantConfig(() -> null);
-    
+
     public static TolerantConfig empty() {
         return empty;
     }
-    
+
     public static TolerantConfig of(ImmutableConfiguration conf) {
         if (conf == null) {
             return empty;
         }
         return new DefaultTolerantConfig<>(() -> conf);
     }
-    
+
     public static <C extends ImmutableConfiguration> TolerantConfig<C> ofSuplier(final ConfigurationSupplier<C> supply) {
         return new DefaultTolerantConfig<>(supply);
     }
-    
+
     public static <C extends ImmutableConfiguration> TolerantConfig<C> ofSuplierCached(final ConfigurationSupplier<C> supply) {
         Objects.requireNonNull(supply, "Supplier must not be null");
         return ofSuplier(new CachingConfSupplier<>(supply));
     }
-    
+
     public static interface ConversionTolerantFunction<C extends ImmutableConfiguration, T> extends Function<C, T> {
-        
+
         @Override
         public default T apply(C t) {
             try {
@@ -99,15 +99,15 @@ public interface TolerantConfig<Conf extends ImmutableConfiguration> extends Imm
                 return null;
             }
         }
-        
+
         public T convert(C t);
-        
+
     }
-    
+
     public static interface ConfigurationSupplier<T extends ImmutableConfiguration> {
-        
+
         public T getConfiguration() throws ConfigurationException;
-        
+
         public default T getConfOrNull() {
             try {
                 return getConfiguration();
@@ -115,28 +115,28 @@ public interface TolerantConfig<Conf extends ImmutableConfiguration> extends Imm
                 return null;
             }
         }
-        
+
         public default boolean isChanged() { // might want to hot reload
             return false;
         }
-        
+
     }
-    
+
     public static class CachingConfSupplier<T extends ImmutableConfiguration> implements ConfigurationSupplier<T> {
-        
+
         protected final ConfigurationSupplier<T> supply;
         protected T conf;
         protected final boolean sync;
-        
+
         public CachingConfSupplier(ConfigurationSupplier<T> real) {
             this(real, true);
         }
-        
+
         public CachingConfSupplier(ConfigurationSupplier<T> real, boolean sync) {
             this.supply = Objects.requireNonNull(real);
             this.sync = sync;
         }
-        
+
         @Override
         public T getConfiguration() throws ConfigurationException {
             if (conf != null && !supply.isChanged()) {
@@ -149,19 +149,19 @@ public interface TolerantConfig<Conf extends ImmutableConfiguration> extends Imm
             } else {
                 conf = supply.getConfOrNull();
             }
-            
+
             return conf;
         }
-        
+
         @Override
         public boolean isChanged() {
             return supply.isChanged();
         }
-        
+
     }
-    
+
     public Conf getDelegated();
-    
+
     public default Iterator<? extends KeyVal> getEntries() {
         ImmutableConfiguration delegated = getDelegated();
         if (delegated == null) {
@@ -169,7 +169,7 @@ public interface TolerantConfig<Conf extends ImmutableConfiguration> extends Imm
         }
         return new KPIterator(delegated.getKeys(), delegated);
     }
-    
+
     public default Iterator<? extends KeyVal> getEntries(String prefix) {
         ImmutableConfiguration delegated = getDelegated();
         if (delegated == null) {
@@ -177,310 +177,326 @@ public interface TolerantConfig<Conf extends ImmutableConfiguration> extends Imm
         }
         return new KPIterator(delegated.getKeys(prefix), delegated);
     }
-    
+
     public default <T> T getByProp(KeyProperty<T> prop) {
         Objects.requireNonNull(prop, "KeyProperty must be supplied");
         return prop.resolve(this);
     }
-    
+
     public default boolean getOr(String key, boolean or) {
         return getBoolean(key, or);
     }
-    
+
     public default int getOr(String key, int or) {
         return getInt(key, or);
     }
-    
+
     public default long getOr(String key, long or) {
         return getLong(key, or);
     }
-    
+
     public default float getOr(String key, float or) {
         return getFloat(key, or);
     }
-    
+
     public default double getOr(String key, double or) {
         return getDouble(key, or);
     }
-    
+
     public default String getOr(String key, String or) {
         return getString(key, or);
     }
-    
+
     public default BigInteger getOr(String key, BigInteger or) {
         return getBigInteger(key, or);
     }
-    
+
     public default BigDecimal getOr(String key, BigDecimal or) {
         return getBigDecimal(key, or);
     }
-    
+
     public default String[] getOr(String key, String[] or) {
         return getStringArray(key, or);
     }
-    
+
     public default <T> T getOr(ConversionTolerantFunction<Conf, T> func, T ifNot) {
         return Optional.ofNullable(getDelegated()).map(func).orElse(ifNot);
     }
-    
+
     public default <T> T getOrSup(ConversionTolerantFunction<Conf, T> func, Supplier<? extends T> ifNot) {
         return Optional.ofNullable(getDelegated()).map(func).orElseGet(ifNot);
     }
-    
+
     public default <T> T getOrThrow(String key, Function<Conf, T> func) {
         return Optional.ofNullable(getDelegated()).map(func).orElseThrow(() -> new NoSuchElementException(key));
     }
-    
+
     @Override
     public default boolean isEmpty() {
         return getOr(ImmutableConfiguration::isEmpty, true);
     }
-    
+
     @Override
     public default int size() {
         return getOr(ImmutableConfiguration::size, 0);
     }
-    
+
     @Override
     public default boolean containsKey(String key) {
         return getOr(p -> p.containsKey(key), false);
     }
-    
+
     @Override
     public default Object getProperty(String key) {
         return getOr(p -> p.getProperty(key), null);
     }
-    
+
     @Override
     public default Iterator<String> getKeys(String prefix) {
         return getOr(p -> p.getKeys(prefix), Collections.EMPTY_LIST.iterator());
     }
-    
+
     @Override
     public default Iterator<String> getKeys() {
         return getOr(ImmutableConfiguration::getKeys, Collections.EMPTY_LIST.iterator());
     }
-    
+
     @Override
     public default Properties getProperties(String key) {
         return getOrSup(p -> p.getProperties(key), Properties::new);
     }
-    
+
     @Override
     public default boolean getBoolean(String key) {
         return getOrThrow(key, p -> p.getBoolean(key));
     }
-    
+
     @Override
     public default boolean getBoolean(String key, boolean defaultValue) {
         return getOr(p -> p.getBoolean(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default Boolean getBoolean(String key, Boolean defaultValue) {
         return getOr(p -> p.getBoolean(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default byte getByte(String key) {
         return getOrThrow(key, p -> p.getByte(key));
     }
-    
+
     @Override
     public default byte getByte(String key, byte defaultValue) {
         return getOr(p -> p.getByte(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default Byte getByte(String key, Byte defaultValue) {
         return getOr(p -> p.getByte(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default double getDouble(String key) {
         return getOrThrow(key, p -> p.getDouble(key));
     }
-    
+
     @Override
     public default double getDouble(String key, double defaultValue) {
         return getOr(p -> p.getDouble(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default Double getDouble(String key, Double defaultValue) {
         return getOr(p -> p.getDouble(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default float getFloat(String key) {
         return getOrThrow(key, p -> p.getFloat(key));
     }
-    
+
     @Override
     public default float getFloat(String key, float defaultValue) {
         return getOr(p -> p.getFloat(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default Float getFloat(String key, Float defaultValue) {
         return getOr(p -> p.getFloat(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default int getInt(String key) {
         return getOrThrow(key, p -> p.getInt(key));
     }
-    
+
     @Override
     public default int getInt(String key, int defaultValue) {
         return getOr(p -> p.getInt(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default Integer getInteger(String key, Integer defaultValue) {
         return getOr(p -> p.getInteger(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default long getLong(String key) {
         return getOrThrow(key, p -> p.getLong(key));
     }
-    
+
     @Override
     public default long getLong(String key, long defaultValue) {
         return getOr(p -> p.getLong(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default Long getLong(String key, Long defaultValue) {
         return getOr(p -> p.getLong(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default short getShort(String key) {
         return getOrThrow(key, p -> p.getShort(key));
     }
-    
+
     @Override
     public default short getShort(String key, short defaultValue) {
         return getOr(p -> p.getShort(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default Short getShort(String key, Short defaultValue) {
         return getOr(p -> p.getShort(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default BigDecimal getBigDecimal(String key) {
         return getOrThrow(key, p -> p.getBigDecimal(key));
     }
-    
+
     @Override
     public default BigDecimal getBigDecimal(String key, BigDecimal defaultValue) {
         return getOr(p -> p.getBigDecimal(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default BigInteger getBigInteger(String key) {
         return getOrThrow(key, p -> p.getBigInteger(key));
     }
-    
+
     @Override
     public default BigInteger getBigInteger(String key, BigInteger defaultValue) {
         return getOr(p -> p.getBigInteger(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default String getString(String key) {
         return getOrThrow(key, p -> p.getString(key));
     }
-    
+
     @Override
     public default String getString(String key, String defaultValue) {
         return getOr(p -> p.getString(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default String getEncodedString(String key, ConfigurationDecoder decoder) {
         return getOrThrow(key, p -> p.getEncodedString(key, decoder));
     }
-    
+
     public default String getEncodedString(String key, ConfigurationDecoder decoder, String defaultValue) {
         return getOr(p -> p.getEncodedString(key, decoder), defaultValue);
     }
-    
+
     @Override
     public default String getEncodedString(String key) {
         return getOrThrow(key, p -> p.getEncodedString(key));
     }
-    
+
     public default String getEncodedString(String key, String defaultValue) {
         return getOr(p -> p.getEncodedString(key), defaultValue);
     }
-    
+
     @Override
     public default String[] getStringArray(String key) {
         return getOrThrow(key, p -> p.getStringArray(key));
     }
-    
+
     public default String[] getStringArray(String key, String[] defaultValue) {
         return getOr(p -> p.getStringArray(key), defaultValue);
     }
-    
+
     @Override
     public default List getList(String key) {
         return getOrThrow(key, p -> p.getList(key));
     }
-    
+
     @Override
     public default List getList(String key, List defaultValue) {
         return getOr(p -> p.getList(key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default <T> T get(Class<T> cls, String key) {
         return getOrThrow(key, p -> p.get(cls, key));
     }
-    
+
     @Override
     public default <T> T get(Class<T> cls, String key, T defaultValue) {
         return getOr(p -> p.get(cls, key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default Object getArray(Class<?> cls, String key) {
         return getOrThrow(key, p -> p.getArray(cls, key));
     }
-    
+
     @Override
     public default Object getArray(Class<?> cls, String key, Object defaultValue) {
         return getOr(p -> p.getArray(cls, key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default <T> List<T> getList(Class<T> cls, String key) {
         return getOrThrow(key, p -> p.getList(cls, key));
     }
-    
+
     @Override
     public default <T> List<T> getList(Class<T> cls, String key, List<T> defaultValue) {
         return getOr(p -> p.getList(cls, key, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default <T> Collection<T> getCollection(Class<T> cls, String key, Collection<T> target) {
         return getOrThrow(key, p -> p.getCollection(cls, key, target));
     }
-    
+
     @Override
     public default <T> Collection<T> getCollection(Class<T> cls, String key, Collection<T> target, Collection<T> defaultValue) {
         return getOr(p -> p.getCollection(cls, key, target, defaultValue), defaultValue);
     }
-    
+
     @Override
     public default TolerantConfig<Conf> immutableSubset(String prefix) {
         return getOr(p -> TolerantConfig.of(p.immutableSubset(prefix)), TolerantConfig.empty);
+    }
+
+    /**
+     * Get all current entries and put into a Properties object which is then
+     * returned.
+     *
+     * @return
+     */
+    public default Properties asProperties() {
+        Properties props = new Properties();
+        Iterator<? extends KeyVal> entries = getEntries();
+        while (entries.hasNext()) {
+            KeyVal kv = entries.next();
+            props.put(kv.getKey(), kv.getValue());
+        }
+        return props;
     }
 
     /**
@@ -492,65 +508,65 @@ public interface TolerantConfig<Conf extends ImmutableConfiguration> extends Imm
      * @param <Conf>
      */
     public static interface TolerantHConfig<Conf extends ImmutableHierarchicalConfiguration> extends TolerantConfig<Conf>, ImmutableHierarchicalConfiguration {
-        
+
         public static final TolerantHConfig empty_h = () -> null;
-        
+
         public static <C extends ImmutableHierarchicalConfiguration> TolerantHConfig<C> empty() {
             return empty_h;
         }
-        
+
         public static <C extends ImmutableHierarchicalConfiguration> TolerantHConfig<C> of(C conf) {
             if (conf == null) {
                 return empty_h;
             }
             return () -> conf;
         }
-        
+
         public static <C extends ImmutableHierarchicalConfiguration> TolerantConfig<C> ofSuplier(final ConfigurationSupplier<C> supply) {
             Objects.requireNonNull(supply, "Supplier must not be null");
             return supply::getConfOrNull;
         }
-        
+
         public static <C extends ImmutableHierarchicalConfiguration> TolerantConfig<C> ofSuplierCached(final ConfigurationSupplier<C> supply) {
             Objects.requireNonNull(supply, "Supplier must not be null");
             return ofSuplier(new CachingConfSupplier<>(supply));
         }
-        
+
         @Override
         public default ExpressionEngine getExpressionEngine() {
             return getOrThrow("ExpressionEngine is not configured", p -> p.getExpressionEngine());
         }
-        
+
         @Override
         public default int getMaxIndex(String key) {
             return getOr(p -> p.getMaxIndex(key), -1);
         }
-        
+
         @Override
         public default String getRootElementName() {
             return getOr(p -> p.getRootElementName(), "");
         }
-        
+
         @Override
         public default TolerantHConfig<Conf> immutableConfigurationAt(String key, boolean supportUpdates) {
             return (TolerantHConfig) getOr(p -> TolerantHConfig.of(p.immutableConfigurationAt(key, supportUpdates)), TolerantHConfig.empty());
         }
-        
+
         @Override
         public default TolerantHConfig<Conf> immutableConfigurationAt(String key) {
             return (TolerantHConfig) getOr(p -> TolerantHConfig.of(p.immutableConfigurationAt(key)), TolerantHConfig.empty());
         }
-        
+
         @Override
         public default List<ImmutableHierarchicalConfiguration> immutableConfigurationsAt(String key) {
             return getOr(p -> p.immutableConfigurationsAt(key), Collections.emptyList());
         }
-        
+
         @Override
         public default List<ImmutableHierarchicalConfiguration> immutableChildConfigurationsAt(String key) {
             return getOr(p -> p.immutableChildConfigurationsAt(key), Collections.emptyList());
         }
-        
+
     }
-    
+
 }
